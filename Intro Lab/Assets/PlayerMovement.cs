688b99b61e8748f16193865bec221868a47183b6
Jason <jasondien@gmail.com>
2020-08-03 07:30:36 -0700

    Copied existing progress

---
+++ Intro Lab/Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public bool dodging = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!dodging) {
+            rb.AddForce(0, 0, 500 * Time.deltaTime);
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision collisionInfo) {
+        if (collisionInfo.collider.tag == "Obstacle") {
+            dodging = true;
+            bool RightOpen = collisionInfo.collider.gameObject.GetComponent<ObstacleInfo>().RightOpen;
+            if (RightOpen) {
+                DodgeRight();
+            }
+            else {
+                DodgeLeft();
+            }
+            dodging = false;
+        }
+
+    }
+
+    void DodgeRight() {
+        rb.AddForce(900, 0, 0);
+    }
+
+    void DodgeLeft() {
+        rb.AddForce(-900, 0, 0);
+    }
+}
