93aabbb84a8f8bd00d5e15b4821a4df7a48eb24a
Jason <jasondien@gmail.com>
2020-08-10 06:36:47 -0700

    Finish first draft for this lab

---
+++ Lab 1 - Simple Obstacle Course/Assets/P1PlayerMovementFinal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1PlayerMovementFinal : MonoBehaviour
+{
+    public Rigidbody rb; // refers to the player GameObject itself
+    
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, 500 * Time.deltaTime);
+    }
+}
